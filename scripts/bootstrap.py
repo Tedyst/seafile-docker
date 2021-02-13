5b1c1b1ed60bd1277b61c72c4c03869ba75bd46f
Stoica Tedy <stoicatedy@gmail.com>
2021-02-13 08:34:03 +0200

    asd

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding: UTF-8
+# coding: UTF-8
 
 """
 Bootstraping seafile server, letsencrypt (verification & cron job).
@@ -27,6 +27,7 @@ shared_seafiledir = '/shared/seafile'
 ssl_dir = '/shared/ssl'
 generated_dir = '/bootstrap/generated'
 
+
 def init_letsencrypt():
     loginfo('Preparing for letsencrypt ...')
     wait_for_nginx()
@@ -50,9 +51,10 @@ def init_letsencrypt():
         loginfo('Found existing cert file {}'.format(ssl_crt))
         if cert_has_valid_days(ssl_crt, 30):
             loginfo('Skip letsencrypt verification since we have a valid certificate')
-	    if exists(join(ssl_dir, 'letsencrypt')):
+            if exists(join(ssl_dir, 'letsencrypt')):
                 # Create a crontab to auto renew the cert for letsencrypt.
-                call('/scripts/auto_renew_crt.sh {0} {1}'.format(ssl_dir, domain))
+                call(
+                    '/scripts/auto_renew_crt.sh {0} {1}'.format(ssl_dir, domain))
             return
 
     loginfo('Starting letsencrypt verification')
@@ -96,8 +98,10 @@ def generate_local_nginx_conf():
         nginx_shared_file = '/shared/nginx/conf/seafile.nginx.conf'
         call('mv {0} {1} && ln -sf {1} {0}'.format(nginx_etc_file, nginx_shared_file))
 
+
 def is_https():
     return get_conf('SEAFILE_SERVER_LETSENCRYPT', 'false').lower() == 'true'
+
 
 def parse_args():
     ap = argparse.ArgumentParser()
@@ -105,30 +109,35 @@ def parse_args():
 
     return ap.parse_args()
 
+
 def init_seafile_server():
     version_stamp_file = get_version_stamp_file()
     if exists(join(shared_seafiledir, 'seafile-data')):
         if not exists(version_stamp_file):
             update_version_stamp(os.environ['SEAFILE_VERSION'])
         # sysbol link unlink after docker finish.
-        latest_version_dir='/opt/seafile/seafile-server-latest'
-        current_version_dir='/opt/seafile/' + get_conf('SEAFILE_SERVER', 'seafile-server') + '-' +  read_version_stamp()
+        latest_version_dir = '/opt/seafile/seafile-server-latest'
+        current_version_dir = '/opt/seafile/' + \
+            get_conf('SEAFILE_SERVER', 'seafile-server') + \
+            '-' + read_version_stamp()
         if not exists(latest_version_dir):
             call('ln -sf ' + current_version_dir + ' ' + latest_version_dir)
-        loginfo('Skip running setup-seafile-mysql.py because there is existing seafile-data folder.')
+        loginfo(
+            'Skip running setup-seafile-mysql.py because there is existing seafile-data folder.')
         return
 
     loginfo('Now running setup-seafile-mysql.py in auto mode.')
     env = {
         'SERVER_NAME': 'seafile',
         'SERVER_IP': get_conf('SEAFILE_SERVER_HOSTNAME', 'seafile.example.com'),
-        'MYSQL_USER': 'seafile',
-        'MYSQL_USER_PASSWD': str(uuid.uuid4()),
+        'MYSQL_USER': get_conf('DB_USERNAME', 'seafile'),
+        'MYSQL_USER_PASSWD': get_conf('DB_PASSWORD', 'password'),
         'MYSQL_USER_HOST': '%.%.%.%',
-	'MYSQL_HOST': get_conf('DB_HOST','127.0.0.1'),
+        'MYSQL_HOST': get_conf('DB_HOST', '127.0.0.1'),
         # Default MariaDB root user has empty password and can only connect from localhost.
         'MYSQL_ROOT_PASSWD': get_conf('DB_ROOT_PASSWD', ''),
     }
+    print(env)
 
     # Change the script to allow mysql root password to be empty
     # call('''sed -i -e 's/if not mysql_root_passwd/if not mysql_root_passwd and "MYSQL_ROOT_PASSWD" not in os.environ/g' {}'''
@@ -136,10 +145,10 @@ def init_seafile_server():
 
     # Change the script to disable check MYSQL_USER_HOST
     call('''sed -i -e '/def validate_mysql_user_host(self, host)/a \ \ \ \ \ \ \ \ return host' {}'''
-        .format(get_script('setup-seafile-mysql.py')))
+         .format(get_script('setup-seafile-mysql.py')))
 
     call('''sed -i -e '/def validate_mysql_host(self, host)/a \ \ \ \ \ \ \ \ return host' {}'''
-        .format(get_script('setup-seafile-mysql.py')))
+         .format(get_script('setup-seafile-mysql.py')))
 
     setup_script = get_script('setup-seafile-mysql.sh')
     call('{} auto -n seafile'.format(setup_script), env=env)
@@ -159,9 +168,11 @@ def init_seafile_server():
 }
 COMPRESS_CACHE_BACKEND = 'locmem'""")
         fp.write('\n')
-        fp.write("TIME_ZONE = '{time_zone}'".format(time_zone=os.getenv('TIME_ZONE',default='Etc/UTC')))
+        fp.write("TIME_ZONE = '{time_zone}'".format(
+            time_zone=os.getenv('TIME_ZONE', default='Etc/UTC')))
         fp.write('\n')
-        fp.write('FILE_SERVER_ROOT = "{proto}://{domain}/seafhttp"'.format(proto=proto, domain=domain))
+        fp.write(
+            'FILE_SERVER_ROOT = "{proto}://{domain}/seafhttp"'.format(proto=proto, domain=domain))
         fp.write('\n')
 
     # By default ccnet-server binds to the unix socket file
@@ -182,11 +193,12 @@ COMPRESS_CACHE_BACKEND = 'locmem'""")
         with open(join(topdir, 'conf', 'seafevents.conf'), 'r') as fp:
             fp_lines = fp.readlines()
             if '[INDEX FILES]\n' in fp_lines:
-               insert_index = fp_lines.index('[INDEX FILES]\n') + 1
-               insert_lines = ['es_port = 9200\n', 'es_host = elasticsearch\n', 'external_es_server = true\n']
-               for line in insert_lines:
-                   fp_lines.insert(insert_index, line)
-    
+                insert_index = fp_lines.index('[INDEX FILES]\n') + 1
+                insert_lines = [
+                    'es_port = 9200\n', 'es_host = elasticsearch\n', 'external_es_server = true\n']
+                for line in insert_lines:
+                    fp_lines.insert(insert_index, line)
+
         with open(join(topdir, 'conf', 'seafevents.conf'), 'w') as fp:
             fp.writelines(fp_lines)
 
@@ -194,7 +206,8 @@ COMPRESS_CACHE_BACKEND = 'locmem'""")
     # container, we need to move them to the shared volume
     #
     # e.g move "/opt/seafile/seafile-data" to "/shared/seafile/seafile-data"
-    files_to_copy = ['conf', 'ccnet', 'seafile-data', 'seahub-data', 'pro-data']
+    files_to_copy = ['conf', 'ccnet',
+                     'seafile-data', 'seahub-data', 'pro-data']
     for fn in files_to_copy:
         src = join(topdir, fn)
         dst = join(shared_seafiledir, fn)
